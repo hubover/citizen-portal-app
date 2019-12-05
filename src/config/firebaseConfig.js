8c2f3aac5c6c3ec235749752aa922cf6e2ca9175
mustafasheikh1 <mustafaahmed726@gmail.com>
2019-12-05 16:28:41 +0500

    changes

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -4,13 +4,13 @@ import 'firebase/auth';
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyBLZ9jEcx1OI-zFPImItXlskTjxnW6rb5I",
-  authDomain: "pm-citizen-portal-4f018.firebaseapp.com",
-  databaseURL: "https://pm-citizen-portal-4f018.firebaseio.com",
-  projectId: "pm-citizen-portal-4f018",
-  storageBucket: "pm-citizen-portal-4f018.appspot.com",
-  messagingSenderId: "712007907005",
-  appId: "1:712007907005:web:b01910c35346aeb8"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 firebase.initializeApp(config);
